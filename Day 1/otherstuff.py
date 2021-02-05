d9d1d256765e25d49588494de440237c493a6333
Toadsta <78148898+Toadsta@users.noreply.github.com>
2021-02-05 17:28:49 +0000

    Create otherstuff.py
    This contains stuff i have learend that isnt in bandname.py

---
+++ Day 1/otherstuff.py
@@ -0,0 +1,5 @@
+#This contains stuff i have learend that isnt in bandname.py
+#if i wanted to get the length of an input in numbers i would do this
+
+#the computer tries to print it straight away but cannot as it has to calcuate the length (len) of the input. len is another function it calcutes how many numbers  
+print(len(input("What is your name?")))
