1db473f90fceca4003fd4578cc76258cb952a858
Toadsta <78148898+Toadsta@users.noreply.github.com>
2021-02-05 18:12:38 +0000

    Update otherstuff.py
    added how to flip variables

--- Day 1/otherstuff.py
+++ Day 1/otherstuff.py
@@ -3,3 +3,17 @@
 
 #the computer tries to print it straight away but cannot as it has to calcuate the length (len) of the input. len is another function it calcutes how many numbers  
 print(len(input("What is your name?")))
+
+#this is a challange it was to make a switch with b and b switch with a without numbers.
+a = input("a: ")
+b = input("b: ")
+
+switcha = a
+switchb = b
+a = switchb
+b = switcha
+
+print("a: " + a)
+print("b: " + b)
+
+
