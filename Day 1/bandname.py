be6b807e674998ac54c1d1aef09c0a2abd752d34
Toadsta <78148898+Toadsta@users.noreply.github.com>
2021-02-05 16:51:02 +0000

    Update bandname.py
    Learned you could give the question from within the input function rather than printing it first

--- Day 1/bandname.py
+++ Day 1/bandname.py
@@ -1,6 +1,5 @@
-print("I am going to make you a band name!\nFirst whats the name of your town?")
-town = input()
-print("Great, now whats the name of your first pet?")
-pet = input()
-print("Hmmmm.... Thinking... Ah yes. The name of your band should be")
+
+town = input("I am going to make you a band name!\nFirst whats the name of your town? ")
+pet = input("Great, now whats the name of your first pet? ")
+print("Hmmmm.... Thinking... Ah yes. The name of your band should be:")
 print(town + pet)
