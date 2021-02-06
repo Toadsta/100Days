304c5a6d1aaec3d1e27cd0ebbadd0f16e1c81312
Toadsta <78148898+Toadsta@users.noreply.github.com>
2021-02-06 21:15:37 +0000

    Create otherstuff.py
    moved

---
+++ 100 Days/Day1/otherstuff.py
@@ -0,0 +1,16 @@
+#This contains stuff i have learend that isnt in bandname.py
+#if i wanted to get the length of an input in numbers i would do this
+
+#the computer tries to print it straight away but cannot as it has to calcuate the length (len) of the input. len is another function it calcutes how many numbers  
+print(len(input("What is your name?")))
+
+#this is a challange it was to make a switch with b and b switch with a without numbers.
+a = input("a: ")
+b = input("b: ")
+
+c = a
+a = b
+b = c
+
+print("a: " + a)
+print("b: " + b)
