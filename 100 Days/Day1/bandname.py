6b67daf316c9f2772ba67a1dc5d5684791934924
Toadsta <78148898+Toadsta@users.noreply.github.com>
2021-02-06 21:15:27 +0000

    Create bandname.py
    Moved

---
+++ 100 Days/Day1/bandname.py
@@ -0,0 +1,8 @@
+# Below i am nesting, the code trys to print but doesnt have an input so asks the user for the name first before saying hello, after the name has been inputed the code is replaced by the name. Then hello is added.
+print("Hello " + input("What is your name? \n"))
+#/n means it starts a new line in the text this means i dont need to write the print command again
+town = input("I am going to make you a band name!\nFirst whats the name of your town? \n")
+pet = input("Great, now whats the name of your first pet? \n")
+print("Hmmmm.... Thinking... Ah yes. The name of your band should be:")
+#this combines the 2 variabes that were marked above
+print(town + pet)
