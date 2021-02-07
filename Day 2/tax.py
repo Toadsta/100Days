d8af7067c6fa712a0daaadacfe3eafb146c404f2
Toadsta <78148898+Toadsta@users.noreply.github.com>
2021-02-07 19:28:23 +0000

    Update tax.py
    Simple Tax calculator

--- Day 2/tax.py
+++ Day 2/tax.py
@@ -1,7 +1,7 @@
 #Also  i changed all int to float just incase of decimals
 print("Welcome to the tip calculator")
 bill = input("What was the total bill? ")
-numberofpeople = input("How many people to split the bill? ")
+people = input("How many people to split the bill? ")
 tip = input("What percentage tip would you like to give? ")
 
 #this calcuates the tax needed to be added to the bill
@@ -9,10 +9,10 @@ tax =  ( ( float(bill) / 100 ) * float(tip) )
 #billtax is bill + tax
 billtax =  float(tax) + float(bill) 
 #this is now going to divide billtax by number of people
-finalpayment = float(billtax) / float(numberofpeople)
+finalpayment = float(billtax) / float(people)
 
 #I added this round function as the bill sometimes came out as a really long string the 2 means to a decial point of 2
 finalpayment = round(finalpayment, 2)
 
 #The str changes the number into a string so I can add the text to the front. If youre unsure of type use print(type("thing"))
-print("Each person should pay $" + str(finalpayment) )
+print(f"Each person should pay ${finalpayment}" )
