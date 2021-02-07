2ae2b2c358840a6885d4dff03488dd87e20029e7
Toadsta <78148898+Toadsta@users.noreply.github.com>
2021-02-07 18:19:49 +0000

    Create otherstuff.py
    Day 2 Learning

---
+++ Day 2/otherstuff.py
@@ -0,0 +1,25 @@
+#data types
+ #string anything in "" text or number
+
+ #int anything that is a whole number
+ #float any number
+ #if we have a large number that is hard to understand we could do this 123_456_789 and the computer would read it as 123456789
+
+ #Boolean means true or false (bool)
+ #In the example below we use == to mean equal to < to mean greater than and > to mean less than, to do equal to or less than add a  
+print(bool( 1 == 1) ) #true
+print(bool( 1 < 1) ) #false
+print(bool( 1 > 1) ) #false
+print(bool( 1 <= 1) ) #true
+print(bool( 1 >= 1) ) #true
+
+#the [] tell the print command which letter of hello to print 0 is H 1 is E etc.
+print("Hello"[0])
+
+#maths python follows bodmas
+1 + 1 #adding
+1 - 1 #subtracting
+1 * 1 #times
+1 / 1 #divide
+1 ** 1 #to the power of
+print(3 * (3 + 3) / 3 - 3 )
