f6db3013da2aa23329a636b08e75cf90cc6f0471
Toadsta <78148898+Toadsta@users.noreply.github.com>
2021-02-07 19:27:35 +0000

    Create f-strings.py
    What are f strings

---
+++ Day 2/f-strings.py
@@ -0,0 +1,11 @@
+#this would have been in other stuff but is quite important
+
+score = 0 #int
+height = 1.8 #float
+isWinning = True #boolean
+
+#normally to convernt these you would have to do str(var) but you can use f see below:
+
+print(f"Your Score is {score}, your height is {height}, and you are winning is {isWinning} ")
+
+#this bit of code automatically converts everything it also means you dont need to use + as its all contained within quotations. 
