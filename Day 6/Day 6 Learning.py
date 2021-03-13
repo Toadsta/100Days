58a193ccfb0c45e5d110e49e61307b9125552a26
Toadsta <78148898+Toadsta@users.noreply.github.com>
2021-03-13 17:11:37 +0000

    Create Day 6 Learning.py

---
+++ Day 6/Day 6 Learning.py
@@ -0,0 +1,25 @@
+#Functions
+#I have already used functions, for example:
+print("hello"); print(len("hello"))
+#we can make our own function! How, we first define (def) a function
+def myFunction():#this colon says anything after this line and indented is part of the function
+    print("Hello")
+    print("Bye")
+#to call the function we do this, if it requires input we put stuff inside the ()
+myFunction()
+#You can put functuions inside of functions to make an easier function for example if you made a turn left function, 
+#you could place 2 of them in a new function and call it turn around
+def myFunction2():
+    #place code here
+    #place code here
+#this code isnt in function
+
+#while loops
+#while loops will continue to run the code until the condition isnt met
+while something: #means when its true
+while not something: # means when not true
+
+#for loops are good for ranges and doing stuff inside a list (iterations)
+
+#while loops are good for when you want to carry out something until conditon is met
+#while loops are more dangerous than for loops as you may accidentaly make an infinite loop
